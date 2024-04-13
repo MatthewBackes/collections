fa2b3fbaaea8932bcea10b9057dda2a5253f45fb
Strom-Cuzewon <mbackes5052@gmail.com>
2024-04-13 00:47:48 -0500

    Testing with hash maps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 enum MultiType{
     Int(i32),
     Float(f64),
@@ -52,5 +54,20 @@ fn main() {
     for i in s4.chars() {
         println!("{i}");
     }
-    
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("Blue team score: {score}");
+    for(key, value) in &scores {
+        println!("{key}: {value}");
+    }
+    scores.insert(String::from("Blue"), 25);
+    println!("{:?}", scores);
+    scores.entry(String::from("Red")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}", scores);
 }
